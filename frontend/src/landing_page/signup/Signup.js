f0dae9ce383839690202330d42545c606a407e48
abhishekkumar71 <151723904+abhishekkumar71@users.noreply.github.com>
2025-04-18 21:58:29 +0530

    updated

--- frontend/src/landing_page/signup/Signup.js
+++ frontend/src/landing_page/signup/Signup.js
@@ -33,7 +33,7 @@ const Signup = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:3002/signup",
+        "https://tradeeasy.onrender.com/signup",
         {
           ...inputValue,
         },
