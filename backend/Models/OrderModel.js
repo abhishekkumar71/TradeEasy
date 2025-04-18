9076aed082addcc1837641e948ce2ea982a360ee
abhishekkumar71 <151723904+abhishekkumar71@users.noreply.github.com>
2025-04-18 14:11:28 +0530

    Properly added backend files

---
+++ backend/Models/OrderModel.js
@@ -0,0 +1,4 @@
+const { model } = require("mongoose");
+const{ OrderSchema } =require( "../Schemas/OrderSchema");
+const  OrderModel  = new model("order", OrderSchema);
+module.exports = { OrderModel };
