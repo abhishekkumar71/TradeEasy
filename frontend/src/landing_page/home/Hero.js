050f5d95b4e76bb177952391c9cd6e466a899aca
abhishekkumar71 <151723904+abhishekkumar71@users.noreply.github.com>
2025-04-19 12:23:50 +0530

    updated styling

--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -4,29 +4,36 @@ function Hero() {
     <>
       <div className="container p-5">
         <div className="row text-center ">
-          <div className="container-fluid">
-            <img 
+          <div
+            className="container-fluid images"
+            style={{
+              width: "100%",
+              marginRight: "5rem",
+              display: "flex",
+              justifyContent: "center",
+            }}
+          >
+            <img
               src="media/images/left.png"
-              className="mb-4"
+              className="mb-4 leftImg"
               alt="left img"
-              style={{ width: "20%",height:"80%",marginTop:"2.6rem" }}
+              style={{ width: "24%", height: "80%", marginTop: "2.6rem" }}
             />
-              <img
+            <img
               src="media/images/watchlist.png"
-              className="mb-4"
+              className="mb-4 centerImg"
               alt="center img"
               style={{ width: "53%" }}
-            />  <img
-            src="media/images/holdings.png"
-            className="mb-4"
-            alt="right img"
-            style={{ width: "25%",height:"80%",marginTop:"2.6rem" }}
-          />
+            />{" "}
+            <img
+              src="media/images/holdings.png"
+              className="mb-4 rightImg"
+              alt="right img"
+              style={{ width: "30%", height: "80%", marginTop: "2.6rem" }}
+            />
           </div>
           <h1 className="mt-5">Invest in everything</h1>
-          <p className="m-4 fs-5">
-            Online platform to invest in stocks
-          </p>
+          <p className="m-4 fs-5">Online platform to invest in stocks</p>
           <button
             className="btn btn-primary fs-5"
             style={{ width: "18%", margin: "0 auto" }}
